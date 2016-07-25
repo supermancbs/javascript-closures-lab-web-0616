1886de80988b2e228bef5642cf8cf4d0d7196edd
supermancbs <clark.sacktor@gmail.com>
2016-07-25 15:27:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  var counter = 0
+
+  function addBump() {return counter+=1}
+
+  function getBumps() {return counter}
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType){
+
+  function deadlyDevice(deadlyDevice) {
+    return {animalType: animalType, deadlyDevice: deadlyDevice}
+  }
+  return deadlyDevice
+}
+
+var sharkCreator = createAnimal("Shark")
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+
+var sharkWithFrickinCannon = sharkCreator('Cannon')
